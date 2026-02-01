8d0ec9f4d54f21f12c8204b858163b93b6d60e6d
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-02-01 22:59:12 +0530

    Create cpp_engine/converter.cpp

---
+++ cpp_engine/converter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+std::string convert(std::string type) {
+    return "C++ converted " + type;
+}
+
+int main() {
+    std::cout << "C++ Engine Ready" << std::endl;
+    return 0;
+}
